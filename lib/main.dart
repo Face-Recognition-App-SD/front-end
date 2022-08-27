8b055172caf9c8ac3d34da9b7f9f6fb0f96dfbfb
zxu0416 <zxu0416@knights.ucf.edu>
2022-08-27 15:08:10 -0400

    add the text box

--- lib/main.dart
+++ lib/main.dart
@@ -5,48 +5,77 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  var background;
+  void anything() {
+    print('anbything');
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          border: Border.all(color: Colors.redAccent),
-          image: DecorationImage(
-            image: AssetImage("assets/images/cat.jpeg"),
-            fit: BoxFit.fitHeight,
-          ),
-        ),
-
-        // EdgeInsets.fromLTRB(left, top, right, bottom)
-
-        //margin: EdgeInsets.all(10),
-        // child: Image.asset(
-        //   'assets/images/logo.jpeg',
-        //   fit: BoxFit.fitWidth,
-        // ),
+      home: Material(
         child: Container(
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Positioned(
-                top: 60,
-                height: 300,
-                width: 300,
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.redAccent),
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/logo.jpeg'),
-                      fit: BoxFit.cover,
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.redAccent),
+            image: DecorationImage(
+              image: AssetImage("assets/images/cat.jpeg"),
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+
+          // EdgeInsets.fromLTRB(left, top, right, bottom)
+
+          //margin: EdgeInsets.all(10),
+          // child: Image.asset(
+          //   'assets/images/logo.jpeg',
+          //   fit: BoxFit.fitWidth,
+          // ),
+          child: Container(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Positioned(
+                  // alignment: Alignment.center,
+                  top: 60,
+                  height: 300,
+                  width: 300,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.redAccent),
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/logo.jpeg'),
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Positioned(
+                  top: 450,
+                  height: 200,
+                  width: 200,
+                  child: Column(
+                    children: [
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter your username',
+                        ),
+                      ),
+                      TextButton(
+                        style: ButtonStyle(
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.blue),
+                        ),
+                        onPressed: anything,
+                        child: Text('blablabla'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
