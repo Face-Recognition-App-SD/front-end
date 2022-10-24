b73e085e62d0444d605939acaaad1bcc9cf88216
Ann Do <ndo5@knights.ucf.edu>
2022-10-24 19:27:43 -0400

    add_bottom_nav

--- lib/screens/patient_list.dart
+++ lib/screens/patient_list.dart
@@ -12,7 +12,7 @@ import '../utils/constant.dart';
 
 
 class PatientList extends StatefulWidget {
-  final token;
+  final String token;
 
   const PatientList({super.key, required this.token});
   State<PatientList> createState() => _PatientList();
@@ -25,7 +25,6 @@ class _PatientList extends State<PatientList> {
   @override
   void initState() {
      token = widget.token;
-    super.initState();
     // initCamera(widget.patients![0]);
   }
 
@@ -46,9 +45,9 @@ class _PatientList extends State<PatientList> {
             ElevatedButton(
               child: Text('Back to HomePage'),
                 onPressed: () async {
-                  Navigator.pop(context);
-            // Navigator.push(context,
-            //     MaterialPageRoute(builder: (_) => Homepage(token: token)));
+                  // Navigator.pop(context);
+            Navigator.push(context,
+                MaterialPageRoute(builder: (_) => Homepage(token: token)));
           },
               )
           ],
@@ -65,7 +64,8 @@ class _PatientList extends State<PatientList> {
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               http.Response resp = snapshot.data as http.Response;
-              print(resp.statusCode);
+              print('token inside showPatient: $token');
+                print(resp.statusCode);
               if (resp.statusCode == 200) {
                   print('uns');
                 final jsonMap = jsonDecode(resp.body);
@@ -97,7 +97,7 @@ class _PatientList extends State<PatientList> {
                           ),
                         ),
                       );
-              } else if (resp.statusCode == 401) { print('npatient');
+              } else if (resp.statusCode == 401) { print('no patient return');
     
                 Future.delayed(Duration.zero, () {
                  
