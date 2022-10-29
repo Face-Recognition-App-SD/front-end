6880fbed79e8707e88264c593ec42dddada410dc
Ann Do <ndo5@knights.ucf.edu>
2022-10-29 06:34:28 -0400

    -notunpdateanything

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,8 +1,8 @@
 class Constants{
   Constants._();
 
-  static const BASE_URL = 'http://192.168.58.214:8000';
+  static const BASE_URL = 'http://192.168.1.80:8000';
 
-  //Ann's clocalhost IP : 192.168.1.80
+  //Ann's localhost IP : 192.168.1.80
   //'https://api.rostro-authentication.com'
 }
