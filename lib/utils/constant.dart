30755938638b9519d1ce9b9b69b1d60751a1d827
Gustavo <53804903+Guzt4vo@users.noreply.github.com>
2022-10-30 10:24:43 -0400

    patient verification

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -3,6 +3,6 @@ class Constants{
 
   //static const BASE_URL = 'http://192.168.58.214:8000';//Ann
   static const BASE_URL = 'http://192.168.1.5:8000';//Gustavo
-  //Ann's clocalhost IP : 192.168.1.80
+
   //'https://api.rostro-authentication.com'
 }
