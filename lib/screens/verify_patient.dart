b53a4314e7fbd21fd03ecb4633316f7379278864
Gustavo <53804903+Guzt4vo@users.noreply.github.com>
2022-11-20 23:52:05 -0500

    Merge remote-tracking branch 'origin/test' into codeImprove
    # Conflicts:
    #	lib/screens/edit_patient.dart

--- lib/screens/verify_patient.dart
+++ lib/screens/verify_patient.dart
@@ -107,7 +107,7 @@ class ExtendVerifyPatient extends State<VerifyPatient> {
                 ScaffoldMessenger.of(context).showSnackBar(snackbar);
               }
               else {
-                const snackbar = SnackBar(content: Text("No Match", textAlign: TextAlign.center, style: TextStyle(fontSize: 20),));
+                const snackbar = SnackBar(content: Text("Match Found!", textAlign: TextAlign.center, style: TextStyle(fontSize: 20),));
                 ScaffoldMessenger.of(context).showSnackBar(snackbar);
                 Uri  getPatientUri = Uri();
                 if(Constants.BASE_URL == "api.rostro-authentication.com"){
